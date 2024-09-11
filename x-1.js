f1f1aae5f1f6ef2220ce8ec9dc4017e458dce15f
Bishuthapa <thapabishu435@gmail.com>
2024-09-11 19:34:55 +0545

    learning js

---
+++ x-1.js
@@ -0,0 +1,12 @@
+function constVowels(input)
+{
+    let count =0;
+    for( const char of input){
+        if(char ==='a' || char ==='e' || char ==='i' || char ==='o' || char ==='u' )
+        {
+            count++;
+        }
+       
+    }
+    console.log(count);
+}
