30988c18dffca03032a4be6cf2262dbba01382f9
Bishuthapa <thapabishu435@gmail.com>
2024-09-14 20:31:05 +0545

    learning js

--- pratices/x-2.js
+++ pratices/x-2.js
@@ -4,3 +4,8 @@ document.getElementById("counter-btn").addEventListener("click", function() {
   count+=1111;
   document.getElementById("count").innerHTML = count;
 });
+//add a function for reset
+document.getElementById("reset").addEventListener("click", function() {
+  count = 0;
+  document.getElementById("count").innerHTML = count;
+})
