3e23fa312db68fc0f856a3f9cddaf8b072dfe79d
Bishuthapa <thapabishu435@gmail.com>
2024-12-08 23:37:20 +0545

    data_types

---
+++ js/dataType.js
@@ -0,0 +1,26 @@
+"use strict";
+//alert (4+4); // we re using nodejs, not browser
+
+
+let name = "Bishu";
+let age = "19";
+let IsLoggedIn = "true";
+let state = null;
+
+/* data type in js
+number =>  2^53
+bigint 
+string => ""
+boolean => true/false
+null => standlone value
+undefined => 
+symbol => unique
+*/
+
+
+//bjects
+
+//typeof()
+
+console.log(typeof undefined);  //undefine
+console.log(typeof null); //objects
