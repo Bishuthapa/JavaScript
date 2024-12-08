252442ea8c6973893a782413861b9cd756b82a00
Bishuthapa <thapabishu435@gmail.com>
2024-12-08 23:50:16 +0545

    convertionOperation

---
+++ js/03_conversionOperation.js
@@ -0,0 +1,20 @@
+let score = "33sdfs";
+
+
+console.log(typeof score); //string
+console.log(typeof score); //string
+
+let valueNUmber = Number(score);
+
+console.log(typeof valueNUmber); //number conversion
+console.log(valueNUmber) //NaN (Not a Number) it is also datat  type
+
+//"33" => 33
+//"33sdfs" => NaN
+//true => 1; false => 0
+
+let IsLoggedIn = 1;
+
+let booleanIsLoggedIn = Boolean(IsLoggedIn);
+console.log(booleanIsLoggedIn); // true
+
