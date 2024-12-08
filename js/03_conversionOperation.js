9b7edf478968d49fbeeeb400753e7ddc780af70d
Bishuthapa <thapabishu435@gmail.com>
2024-12-09 00:10:55 +0545

    operation file

--- js/03_conversionOperation.js
+++ js/03_conversionOperation.js
@@ -1,4 +1,4 @@
-let score = "33sdfs";
+/*let score = "33sdfs";
 
 
 console.log(typeof score); //string
@@ -23,5 +23,29 @@ let someNumber = 33;
 
 let stringNumber = String(someNumber);
 console.log(stringNumber);
-console.log(typeof stringNumber);
+console.log(typeof stringNumber);*/
 
+/**************************************Oprations**************************/
+
+
+let value = 33;
+let negativeValue = -value;
+//console.log(negativeValue);
+
+let str1 = "bishu"
+let str2 = " thapa"
+let str3 = str1 + str2;
+//console.log(str3);
+
+
+
+//console.log(1 + "2");//12
+
+//console.log("1" + 2 +2 ); // 122
+
+//console.log(1 + 2 + "2"); //32
+
+
+let gameCounter = 100;
+gameCounter++;
+//console.log(gameCounter); // increment
