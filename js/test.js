d488194c0fa0b1aa19882913037d1550a600acbb
Bishuthapa <thapabishu435@gmail.com>
2024-12-08 23:15:38 +0545

    make a table in console

--- js/test.js
+++ js/test.js
@@ -1,1 +1,4 @@
-console("Hello, World!");
+const accountID = 23434
+let accountEmail = "bHt0k@example.com"
+
+console.table([accountID, accountEmail]) // [ 23434, 'bHt0k@example.com' ] (accountID, accountEmail)
