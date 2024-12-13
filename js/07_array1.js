882b81f8ae4ade97e6826d19755e5c5351120842
Bishuthapa <thapabishu435@gmail.com>
2024-12-13 23:59:12 +0545

    array in js

---
+++ js/07_array1.js
@@ -0,0 +1,30 @@
+const myArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+//console.log(myArray[0]);
+//myArray.push(10);
+//console.log(myArray);
+//myArray.pop();
+//console.log(myArray);
+
+
+//myArray.unshift(5);
+//myArray.shift();
+//console.log(myArray);
+
+//console.log(myArray.includes(9));
+//console.log(myArray.indexOf(9));
+
+
+
+//const newAre = myArray.join()
+//
+//console.log(myArray);
+//console.log(newAre);
+
+console.log("A", myArray);
+
+const myn1 =myArray.slice(1,3)
+console.log(myn1);
+console.log("B ", myArray);
+
+const myn2 = myArray.splice(1,3);
+console.log (myn2);
