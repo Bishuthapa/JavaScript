ca70c305b4e917f43244ef7ada48609d2342b420
Bishuthapa <thapabishu435@gmail.com>
2024-12-13 23:35:23 +0545

    data and time in js

---
+++ js/06_dateAndTime.js
@@ -0,0 +1,29 @@
+let newDate = new Date();
+//console.log(newDate.toDateString());
+//console.log(newDate.toLocaleString());
+//console.log(newDate.toLocaleDateString()); 
+
+
+let sameDate = new Date("2005-02-07");
+//console.log(sameDate.toLocaleString());
+
+
+
+let timeStamp= Date.now();
+
+//console.log(timeStamp);
+//console.log(sameDate.getTime());
+//console.log(Math.floor(sameDate.getTime()/1000));
+
+
+
+let nDate = new Date()
+//console.log(nDate.toLocaleString());
+//console.log("date:" +  nDate.getDate());
+//console.log("day:" +  nDate.getDay());
+//console.log("hours:" +  nDate.getHours());
+//console.log(nDate.getMonth() + 1);
+
+
+
+
