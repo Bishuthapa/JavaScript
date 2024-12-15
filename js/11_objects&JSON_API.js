c5bd232c2369e82dcfa43769bc23199a8554b883
Bishuthapa <thapabishu435@gmail.com>
2024-12-15 16:09:11 +0545

    objects and JSON

---
+++ js/11_objects&JSON_API.js
@@ -0,0 +1,74 @@
+const course = {
+    courseName : "JavaScript",
+    price : 999,
+    courseInstructor : "Bishesh",
+    courseLevel : "Beginner"
+}
+
+console.log(course.courseInstructor);
+const {courseInstructor,courseName,price,courseLevel} = course
+//console.log(courseInstructor);
+//console.log(courseName);
+//console.log(price);
+//console.log(courseLevel);
+
+
+
+//{
+//    "name" : "bibit",
+//    "age" : 17,
+//    "grade" : 12,
+//    "course" : "computer"
+//}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
