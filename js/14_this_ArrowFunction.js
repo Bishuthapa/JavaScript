114df472bf5d1573a3e0ce164c58f2062e26dd49
Bishuthapa <thapabishu435@gmail.com>
2024-12-17 13:09:30 +0545

    this and arrow functions

---
+++ js/14_this_ArrowFunction.js
@@ -0,0 +1,25 @@
+function one (){
+    let userName = "Bishesh";
+    //console.log(this);
+}
+one();//window
+
+const two =  () => {
+    let userName = "Bishesh";
+    //console.log(this.userName);//undefined in arrow function
+}
+
+
+const addTwo = (num1, num2) => {
+    return num1 + num2;
+
+}
+// console.log(addTwo(3,4));
+
+
+//const addThree = (num1, num2) => (num1 + num2);
+const addThree = (num1, num2) => ({userName: "Bishesh"});
+
+
+
+
