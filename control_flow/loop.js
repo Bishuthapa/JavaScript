d9eaf7035107d235ad494add821ab57748ea5c13
Bishuthapa <thapabishu435@gmail.com>
2024-12-19 21:58:38 +0545

    initial commit

---
+++ control_flow/loop.js
@@ -0,0 +1,11 @@
+for(let index = 0; index < 10; index++){
+    const element =index;
+    //console.log(element);
+
+}
+
+let myArray = ["bibit", "joshan","bishu"]
+
+for(let i = 0 ; i < myArray.length; i++){
+    console.log(myArray[i]);
+}
