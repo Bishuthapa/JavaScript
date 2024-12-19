81de87bfeb157984813b17c6a9ffb378ca591033
Bishuthapa <thapabishu435@gmail.com>
2024-12-19 22:10:46 +0545

    initial commit

--- control_flow/loop.js
+++ control_flow/loop.js
@@ -1,3 +1,4 @@
+//for loop
 for(let index = 0; index < 10; index++){
     const element =index;
     //console.log(element);
@@ -9,3 +10,21 @@ let myArray = ["bibit", "joshan","bishu"]
 for(let i = 0 ; i < myArray.length; i++){
     console.log(myArray[i]);
 }
+
+
+//while loop
+
+let index = 0;
+while(index < myArray.length)
+{
+    //console.log(`my array is ${myArray[index]}`);
+    index++;
+}
+
+//do while loop
+let i = 0;
+
+do{
+    console.log(`print me ${myArray[i]}`);
+    i++;
+}while(i<myArray.length)
