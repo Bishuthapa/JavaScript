dddd42e9cbbc358b7d5f0e8074d418def03099b9
Bishuthapa <thapabishu435@gmail.com>
2024-12-26 17:22:36 +0545

    Events

---
+++ Events/scritp.js
@@ -0,0 +1,6 @@
+const container = document.getElementById('container');
+const button = document.getElementById('button');
+
+button.addEventListener('click', () => {
+  container.innerHTML = 'Hello World';
+}); 
